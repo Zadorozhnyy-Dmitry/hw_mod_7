6dbf734fec108e76c7c8c245407a6b8e5922b14e
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-10-30 00:26:30 +0300

    Создана модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from config.settings import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+
+    phone = models.CharField(
+        max_length=35, verbose_name="Телефон", help_text="Укажите телефон", **NULLABLE
+    )
+    city = models.CharField(
+        max_length=35, verbose_name="Город", help_text="Укажите город", **NULLABLE
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        verbose_name="Аватар",
+        help_text="Загрузите фото",
+        **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
