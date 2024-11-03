f6065d6be52a527eaa0f78deeb8873bfec6d9f5b
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-11-03 22:36:15 +0300

    hw_24.2 эндпоинт для редактирования профиля пользователя на основе Viewset

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from users.models import User
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+
+class UserViewSet(ModelViewSet):
+    """
+    Контроллер для пользователя
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
