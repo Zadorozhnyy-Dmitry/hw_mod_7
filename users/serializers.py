ddaca4ae6805802a73e1ee0031ad9bd4f66b5afe
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-11-05 20:34:08 +0300

    hw_24.2 Для профиля пользователя реализован вывод платежей

--- users/serializers.py
+++ users/serializers.py
@@ -3,16 +3,6 @@ from rest_framework.serializers import ModelSerializer
 from users.models import Payment, User
 
 
-class UserSerializer(ModelSerializer):
-    """
-    Сериализатор для пользователя
-    """
-
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
 class PaymentSerializer(ModelSerializer):
     """
     Сериализатор для платежа
@@ -20,4 +10,15 @@ class PaymentSerializer(ModelSerializer):
 
     class Meta:
         model = Payment
+        fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    """
+    Сериализатор для пользователя
+    """
+    payments = PaymentSerializer(source='payment_set', many=True)
+
+    class Meta:
+        model = User
         fields = "__all__"
